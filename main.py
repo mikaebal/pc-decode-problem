151260a1da9abd9d8af4861313c715e5732f66e1
Ansel Rognlie <ansel@adadevelopersacademy.org>
2025-01-15 15:06:22 -0800

    initial commit ported from replit

---
+++ main.py
@@ -0,0 +1,10 @@
+def decode(code):
+    pass
+
+
+assert (decode('12106')) == 2
+assert (decode('339')) == 1
+assert (decode('306')) == 0
+
+print("All tests passed!")
+print("Discuss time & space complexity if time remains.")
